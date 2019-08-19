71fded11f059ea6a4edc1f5da66f806165789df8
PaulVoit <paulv2007@yandex.ru>
2019-08-20 00:20:24 +0300

    Отладка программы в IDEA [#163085]

---
+++ chapter_001/src/test/java/SqMaxTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+import ru.job4j.condition.SqMax;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SqMaxTest {
+	@Test
+	public void whenFirstMax() {
+		SqMax check = new SqMax();
+		int result = check.max(8, 3, 2, 6);
+		assertThat(result, is(8));
+	}
+	@Test
+	public void whenSecondMax() {
+		SqMax check = new SqMax();
+		int result = check.max(3, 7, 2,6);
+		assertThat(result, is(7));
+	}
+	@Test
+	public void whenThirdMax() {
+		SqMax check = new SqMax();
+		int result = check.max(10, 2, 12, 6);
+		assertThat(result, is(12));
+	}
+	@Test
+	public void whenForthMax() {
+		SqMax check = new SqMax();
+		int result = check.max(3, 2, 9, 12);
+		assertThat(result, is(12));
+	}
+	@Test
+	public void whenAllEqual() {
+		SqMax check = new SqMax();
+		int result = check.max(3, 3, 3, 3);
+		assertThat(result, is(3));
+	}
+}
