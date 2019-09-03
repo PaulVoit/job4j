76db1663925543dff0051a9450a0dfd68cd7e4df
PaulVoit <paulv2007@yandex.ru>
2019-09-03 21:41:12 +0300

    Сортировка выборкой. [#163111]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -24,4 +24,21 @@ public class FindLoop {
 		}
 		return rst;
 	}
+
+	public int[] sort(int[] data) {
+		for (int i = 0; i < data.length; i++) {
+			int min = data[i];
+			int minId = i;
+			for (int j = i + 1; j < data.length; j++) {
+				if (data[j] < min) {
+					min = data[j];
+					minId = j;
+				}
+			}
+			int temp = data[i];
+			data[i] = min;
+			data[minId] = temp;
+		}
+		return data;
+	}
 }
