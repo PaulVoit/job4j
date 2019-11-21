2698a2f493b92d2f06567b20a262a6961a4df25d
PaulVoit <paulv2007@yandex.ru>
2019-11-22 00:12:08 +0300

    5. Рефакторинг теста @Before @After[#163193]

---
+++ chapter_002/src/test/java/PaintTest.java
@@ -0,0 +1,69 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ru.job4j.strategy.Paint;
+import ru.job4j.strategy.Square;
+import ru.job4j.strategy.Triangle;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PaintTest {
+
+	private final PrintStream stdout = System.out;
+
+	private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+	@Before
+	public void loadOutput() {
+		System.out.println("execute before method");
+		System.setOut(new PrintStream(this.out));
+	}
+
+	@After
+	public void backOutput() {
+		System.setOut(this.stdout);
+		System.out.println("execute after method");
+	}
+
+	@Test
+	public void whenDrawSquare() {
+		new Paint().draw(new Square());
+		assertThat(
+				this.out.toString(),
+				is(
+						new StringBuilder()
+								.append("_______").append(System.lineSeparator())
+								.append("|     |").append(System.lineSeparator())
+								.append("|     |").append(System.lineSeparator())
+								.append("_______").append(System.lineSeparator())
+								.append(System.lineSeparator())
+								.toString()
+				)
+		);
+	}
+
+	@Test
+	public void whenDrawTriangle() {
+		new Paint().draw(new Triangle());
+		assertThat(
+				this.out.toString(),
+				is(
+						new StringJoiner(
+								System.lineSeparator(), "",
+								System.lineSeparator())
+								.add("    0")
+								.add("   0 0")
+								.add("  0   0")
+								.add(" 0     0")
+								.add("000000000")
+								.toString()
+				)
+		);
+	}
+}
