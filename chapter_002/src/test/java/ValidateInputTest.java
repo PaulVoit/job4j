bdad22973467f0b3201823f18446e90b97d97f91
PaulVoit <paulv2007@yandex.ru>
2019-11-26 00:16:54 +0300

    Рефакторинг - Шаблон Декоратор для валидатора.[#163201]

--- chapter_002/src/test/java/ValidateInputTest.java
+++ chapter_002/src/test/java/ValidateInputTest.java
@@ -4,9 +4,9 @@ import ru.job4j.tracker.ValidateInput;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
 
 
 public class ValidateInputTest {
@@ -16,12 +16,28 @@ public class ValidateInputTest {
 		PrintStream out = System.out;
 		System.setOut(new PrintStream(mem));
 		ValidateInput input = new ValidateInput(
-				new StubInput(new String[] {"invalid", "1"})
+				new StubInput(new String[]{"invalid", "1"})
 		);
 		input.askInt("Enter", 1);
 		assertThat(
-				mem,
+				mem.toString(),
 				is(String.format("Please enter validate data again ")));
+		System.setOut(out);
+	}
+
+	@Test
+	public void whenInvalidInputKey() {
+		ByteArrayOutputStream mem = new ByteArrayOutputStream();
+		PrintStream out = System.out;
+		System.setOut(new PrintStream(mem));
+		ValidateInput input = new ValidateInput(
+				new StubInput(new String[]{"7", "1"})
+		);
+
+		input.askInt("Enter", 1);
+		assertThat(
+				mem.toString(),
+				is(String.format("Please select key from menu %n")));
 		System.setOut(out);
 	}
 }
